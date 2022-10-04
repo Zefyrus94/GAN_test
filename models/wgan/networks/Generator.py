ca16961c2698eb2fc76b9dda3615d1809c615776
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-10-04 17:28:54 +0200

    wgan - gen disc

---
+++ models/wgan/networks/Generator.py
@@ -0,0 +1,41 @@
+#si ripete anche in train.py
+import torch
+from torch import nn
+class Generator(nn.Module):
+    def __init__(self, input_dim=10, im_chan=3, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.input_dim = input_dim
+        # Build the neural network
+        self.gen = nn.Sequential(
+            self.make_gen_block(input_dim, hidden_dim * 8),#input_dim => hidden_dim * 4
+            #
+            self.make_gen_block(hidden_dim * 8, hidden_dim * 4),
+            #
+            #, kernel_size=4, stride=1
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
+
+    def unsqueeze_noise(self, noise):
+        return noise.view(len(noise), self.input_dim, 1, 1)    
+    def forward(self, noise):
+        x = self.unsqueeze_noise(noise)
+        return self.gen(x)
+#print("preambolo...")
+#from torchsummary import summary
+#gen = Generator(64).to('cpu')
+#summary(gen, (64,))

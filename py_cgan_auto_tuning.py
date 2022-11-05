073bc01c5abed6f772251f89078ccd28ad9ee554
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-05 02:09:58 +0100

    ValueError: Unknown argument found in the Trainable function. The function args must include a 'config' positional parameter. Any other args must be 'checkpoint_dir'. Found: ['gen', 'disc', 'dataloader', 'loss_f']
    train(config,gen,disc,dataloader,loss_f)

--- py_cgan_auto_tuning.py
+++ py_cgan_auto_tuning.py
@@ -206,7 +206,7 @@ def weights_init(m):
     if isinstance(m, nn.BatchNorm2d):
         torch.nn.init.normal_(m.weight, 0.0, 0.02)
         torch.nn.init.constant_(m.bias, 0)
-def train(gen,disc,dataloader,loss_f):
+def train(config,gen,disc,dataloader,loss_f):
     # UNQ_C4 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
     # GRADED CELL
     cur_step = 0

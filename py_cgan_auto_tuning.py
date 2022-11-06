235c81553c8b96890a160a5d85996f4ac886520b
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-06 12:38:26 +0100

    vae

--- py_cgan_auto_tuning.py
+++ py_cgan_auto_tuning.py
@@ -24,6 +24,86 @@ import numpy as np
 import copy
 torch.manual_seed(0) # Set for our testing purposes, please do not change!
 print("cuda available?",torch.cuda.is_available())
+####per la valutazione: VAE...
+"""
+Encoder(
+  (conv1): Conv2d(1, 64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
+  (conv2): Conv2d(64, 128, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
+  (fc_mu): Identity()
+  (fc_logvar): Identity()
+)
+----------------------------------------------------------------
+        Layer (type)               Output Shape         Param #
+================================================================
+            Conv2d-1           [-1, 64, 14, 14]           1,088
+            Conv2d-2            [-1, 128, 7, 7]         131,200
+          Identity-3                 [-1, 6272]               0
+          Identity-4                 [-1, 6272]               0
+================================================================
+Total params: 132,288
+Trainable params: 132,288
+Non-trainable params: 0
+----------------------------------------------------------------
+Input size (MB): 0.00
+Forward/backward pass size (MB): 0.24
+Params size (MB): 0.50
+Estimated Total Size (MB): 0.75
+----------------------------------------------------------------
+"""
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        c = capacity
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=c, kernel_size=4, stride=2, padding=1) # out: c x 14 x 14
+        self.conv2 = nn.Conv2d(in_channels=c, out_channels=c*2, kernel_size=4, stride=2, padding=1) # out: c x 7 x 7
+        self.fc_mu = nn.Linear(in_features=c*2*7*7, out_features=latent_dims)
+        self.fc_logvar = nn.Linear(in_features=c*2*7*7, out_features=latent_dims)
+            
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(x.size(0), -1) # flatten batch of multi-channel feature maps to a batch of feature vectors
+        x_mu = self.fc_mu(x)
+        x_logvar = self.fc_logvar(x)
+        return x_mu, x_logvar
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        c = capacity
+        self.fc = nn.Linear(in_features=latent_dims, out_features=c*2*7*7)
+        self.conv2 = nn.ConvTranspose2d(in_channels=c*2, out_channels=c, kernel_size=4, stride=2, padding=1)
+        self.conv1 = nn.ConvTranspose2d(in_channels=c, out_channels=1, kernel_size=4, stride=2, padding=1)
+            
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(x.size(0), capacity*2, 7, 7) # unflatten batch of feature vectors to a batch of multi-channel feature maps
+        x = F.relu(self.conv2(x))
+        x = torch.sigmoid(self.conv1(x)) # last layer before output is sigmoid, since we are using BCE as reconstruction loss
+        return x
+    
+class VariationalAutoencoder(nn.Module):
+    def __init__(self):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+    
+    def forward(self, x):
+        latent_mu, latent_logvar = self.encoder(x)
+        latent = self.latent_sample(latent_mu, latent_logvar)
+        x_recon = self.decoder(latent)
+        return x_recon, latent_mu, latent_logvar
+    
+    def latent_sample(self, mu, logvar):
+        if self.training:
+            # the reparameterization trick
+            std = logvar.mul(0.5).exp_()
+            eps = torch.empty_like(std).normal_()
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+####fine vae
 class Generator(nn.Module):
     '''
     Generator Class
@@ -470,7 +550,7 @@ def train(config):
             #    path = os.path.join(checkpoint_dir, "checkpoint")
             #    torch.save((net.state_dict(), optimizer.state_dict()), path)
             #prove fid
-            fid = get_fid(gen)#randrange(10)
+            fid = randrange(10)#get_fid(gen)#
             tune.report(fid=fid, loss_d=(running_loss_d / num_of_batches), loss_g=(running_loss_g / num_of_batches))
         print(f'[Epoch {epoch + 1}/{n_epochs}] loss d: {running_loss_d / num_of_batches:.3f}; loss g: {running_loss_g / num_of_batches:.3f}')
         loss_f.write(f"{running_loss_d / num_of_batches:.3f};{running_loss_g / num_of_batches:.3f}\n")

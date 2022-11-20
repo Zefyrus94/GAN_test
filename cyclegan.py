5cbfce3ff0525470b484082fe3b559591175d00e
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-20 10:46:32 +0100

    save numpy

--- cyclegan.py
+++ cyclegan.py
@@ -22,7 +22,7 @@ def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28), img_name=N
     image_shifted = image_tensor
     image_unflat = image_shifted.detach().cpu().view(-1, *size)
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
-    img_to_save = image_grid.permute(1, 2, 0).squeeze()
+    img_to_save = image_grid.permute(1, 2, 0).squeeze().cpu().numpy()
     if img_name!= None:
         plt.imsave(img_name, img_to_save)
     #.imshow(image_grid.permute(1, 2, 0).squeeze())

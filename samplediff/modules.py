bd91086e80135c642f17fd7b27aee9a7d0c208dd
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-24 17:18:38 +0100

    model parallel

--- samplediff/modules.py
+++ samplediff/modules.py
@@ -7,9 +7,22 @@ class UNet(nn.Module):
     def __init__(self, c_in=3, c_out=3, time_dim=256, device="cuda:0"):
         super().__init__()
         self.inc = nn.Conv2d(c_in, c_out, kernel_size=3, padding=1, bias=False).to('cuda:2')
-
+    def pos_encoding(self, t, channels):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
+        )
+        #print("pos_encoding dev t",t.device.index," inv_freq,",inv_freq.device.index)
+        print("pos_encoding",t.shape)
+        print(t,inv_freq)
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc
     def forward(self, x, t):
-        t = t.to('cuda:2')
+        t = t.unsqueeze(-1).type(torch.float)
+        t = self.pos_encoding(t, self.time_dim)
+        #t = t.to('cuda:2')
         x = x.to('cuda:2')
         print("x dev",x.device)
         x1 = self.inc(x)

c2ddb7d865c27fa6c14a11d621ba4a9c437b6e00
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-24 01:04:04 +0100

    .to('cuda:2')

--- samplediff/modules.py
+++ samplediff/modules.py
@@ -62,11 +62,11 @@ class DoubleConv(nn.Module):
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False).to('cuda:2'),
-            nn.GroupNorm(1, mid_channels).to('cuda:2'),
-            nn.GELU().to('cuda:2'),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False).to('cuda:2'),
-            nn.GroupNorm(1, out_channels).to('cuda:2'),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False)#.to('cuda:2'),
+            nn.GroupNorm(1, mid_channels)#.to('cuda:2'),
+            nn.GELU()#.to('cuda:2'),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False)#.to('cuda:2'),
+            nn.GroupNorm(1, out_channels)#.to('cuda:2'),
         )
 
     def forward(self, x):

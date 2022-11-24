103ec77c70ac8737f91f3569cc891e11c1ab1bb0
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-24 17:04:26 +0100

    model parallel

--- samplediff/modules.py
+++ samplediff/modules.py
@@ -6,10 +6,10 @@ import torch.nn.functional as F
 class UNet(nn.Module):
     def __init__(self, c_in=3, c_out=3, time_dim=256, device="cuda:0"):
         super().__init__()
-        self.inc = nn.Conv2d(c_in, c_out, kernel_size=3, padding=1, bias=False).to('cuda:2')
+        self.inc = nn.Conv2d(c_in, c_out, kernel_size=3, padding=1, bias=False)
 
     def forward(self, x, t):
-        x.to('cuda:2')
+        x = x.to('cuda:2')
         print("x dev",x.device)
         x1 = self.inc(x)
         output = x1

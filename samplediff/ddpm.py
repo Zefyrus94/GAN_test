43fe2dd7a0ace64f4ef4e9fe328413c641d3bc30
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-25 10:37:00 +0100

    model sharding

--- samplediff/ddpm.py
+++ samplediff/ddpm.py
@@ -117,7 +117,7 @@ def train(args):
 
             pbar.set_postfix(MSE=loss.item())
             logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
-            sampled_images = diffusion.sample(model, n=images.shape[0])
+            #sampled_images = diffusion.sample(model, n=images.shape[0])
 
         sampled_images = diffusion.sample(model, n=images.shape[0])
         save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))

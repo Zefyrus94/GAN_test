b10f2b8d7d34d8c2242b8e303fe051296607ffe4
Zefyrus94 <giacomo.bonanni94@gmail.com>
2022-11-25 14:13:14 +0100

    model parallel

--- model_parallel.py
+++ model_parallel.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
+#
+import numpy as np
+import timeit
+#
 print("model parallel example")
 class ToyModel(nn.Module):
     def __init__(self):
@@ -23,16 +26,62 @@ class ToyModel(nn.Module):
 # need to make sure that the labels are on the same device as the outputs when
 # calling the loss function.
 
+class ToyModelSplit(nn.Module):
+    def __init__(self, split_size=20):
+        super(ToyModel, self).__init__()
+        self.split_size = split_size
+        self.net1 = torch.nn.Linear(10, 10).to('cuda:2')
+        self.relu = torch.nn.ReLU()
+        self.net2 = torch.nn.Linear(10, 5).to('cuda:3')
 
-model = ToyModel()
-from torchsummary import summary
-summary(model, (20,10))
+    def forward(self, x):
+		splits = iter(x.split(self.split_size, dim=0))
+		s_next = next(splits)
+		s_prev = self.relu(self.net1(x.to('cuda:2')))
+		ret = []
 
-loss_fn = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001)
+		for s_next in splits:
+		    # A. s_prev runs on cuda:1
+		    s_prev = self.net2(x.to('cuda:3'))
+		    ret.append(s_prev)
 
-optimizer.zero_grad()
-outputs = model(torch.randn(20, 10))
-labels = torch.randn(20, 5).to('cuda:1')
-loss_fn(outputs, labels).backward()
-optimizer.step()
+		    # B. s_next runs on cuda:2, which can run concurrently with A
+		    s_prev = self.relu(self.net1(x.to('cuda:2')))
+
+		s_prev = self.net2(s_prev.to('cuda:3'))
+		ret.append(s_prev)
+
+		return torch.cat(ret)
+
+def train(model):
+	loss_fn = nn.MSELoss()
+	optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+	optimizer.zero_grad()
+	outputs = model(torch.randn(20, 10))
+	labels = torch.randn(20, 5).to('cuda:1')
+	loss_fn(outputs, labels).backward()
+	optimizer.step()
+	
+is_split = True
+if not is_split:
+	model = ToyModel()
+	from torchsummary import summary
+	summary(model, (20,10))
+
+	loss_fn = nn.MSELoss()
+	optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+	optimizer.zero_grad()
+	outputs = model(torch.randn(20, 10))
+	labels = torch.randn(20, 5).to('cuda:1')
+	loss_fn(outputs, labels).backward()
+	optimizer.step()
+else:
+	print("split...")
+	stmt = "train(model)"
+	setup = "model = ToyModelSplit()"
+	pp_run_times = timeit.repeat(
+	    stmt, setup, number=1, repeat=num_repeat, globals=globals())
+	pp_mean, pp_std = np.mean(pp_run_times), np.std(pp_run_times)
+	print(pp_run_times,pp_mean,pp_std)
